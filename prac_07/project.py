b94c0efe75bee4c1e9068b901ddbd58d5c125f22
asmitasharma01 <asmita.sharma@my.jcu.edu.au>
2025-03-21 17:26:51 +0800

    Refactor Project class constructor to store start_date as a datetime object

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,7 +3,7 @@ import datetime
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y")
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
