8066782be6f3210d0dfc99dc86a3ed46f86ad7d4
asmitasharma01 <asmita.sharma@my.jcu.edu.au>
2025-04-04 09:32:33 +0800

    Rename test_taxi to main and improve docstrings for clarity

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,15 +4,25 @@ restarts the meter, drives it again, and prints the details and current fare."""
 from taxi import Taxi
 
 
-def test_taxi():
-    """Test taxi class."""
+def main():
+    """create a new taxi object."""
     my_taxi = Taxi("Prius 1", 100)
-    print(my_taxi)
+
+    # drive the taxi 40 km
     my_taxi.drive(40)
-    print(my_taxi)
-    print(f"Current fare: ${my_taxi.get_fare():.2f}")
-    my_taxi.start_fare()
-    my_taxi.drive(100)
+
+    # Print the taxi's details and the current fare
     print(my_taxi)
     print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
+    # Restart the meter (start a new fare) and then drive the car 100 km
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
+    # Print the details and the current fare again
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+
+if __name__ == "__main__":
+    main()
